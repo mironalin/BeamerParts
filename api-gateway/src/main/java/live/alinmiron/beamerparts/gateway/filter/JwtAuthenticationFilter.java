bfe8197d165d926e7c909ba1b6e435bc8f0368c6
mironalin <mironalin22@stud.ase.ro>
2025-08-12 00:56:44 +0300

    refactor: Remove unused HttpStatus import from JwtAuthenticationFilter

--- api-gateway/src/main/java/live/alinmiron/beamerparts/gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/live/alinmiron/beamerparts/gateway/filter/JwtAuthenticationFilter.java
@@ -3,7 +3,6 @@ package live.alinmiron.beamerparts.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
